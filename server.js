b3d65bc9dba1012a1347444430f4b9c01fb14ceb
Amine <aelqaraoui101@gmail.com>
2021-11-17 10:50:56 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const URL = "https://www.arweave.net"
+
+app.get('/metadata/:id', (req, res) => {
+    let id = req.params.id
+    res.json({
+      "name":"PLAIN CANVAS",
+      "symbol":"PLAIN",
+      "seller_fee_basis_points":1500,
+      "image": URL + "/image/" + id,
+      "attributes":[
+        {"trait_type":"CANVAS ID","value":id}
+      ],
+      "properties":{
+        "creators":[{"address":"Ao85BgbPP2kTxzXjbGEksbTm5TZgXrEQyBujGSoF5jhR","share":100}],
+        "files":[{"uri": URL + "/image/" + id,"type":"image/png"}]
+      }
+    });
+})
+
+app.get('/image/:id', (req, res) => {
+  let id = req.params.id
+  res.sendFile('/home/amine/Projects/NFT/PLAINCANVAS/PLAIN.png');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
